6221f00cacd38c7a9bd9714024cbd6f32a722818
Student <student@Admins-MacBook-Air.local>
2018-07-09 15:03:36 -0400

    shittles

--- hello.js
+++ hello.js
@@ -1,1 +1,4 @@
-// Author: FirstName LastName
+// Author: Shauntiara Williams
+
+console.log('Hello Mouth breather');
+console.log('Im in a code next group with people who make me feel strange @_@ help...' );
