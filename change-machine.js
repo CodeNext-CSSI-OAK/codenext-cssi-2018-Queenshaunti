cd6e889fbef105d4805a3da376d657f41bd7017f
Student <student@Admins-MacBook-Air.local>
2018-07-10 20:01:59 -0400

    Change Machine

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,18 @@
-// Author: FirstName LastName
+// Author: Shautiara Williams
+let readline = require('readline-sync');
+
+let cents = Number(readline.question('Please enter cents as a positive integer: '));
+let quarters = Math.floor(cents / 25);
+console.log('quarters =' + quarters);
+
+let remainderDimes = Math.floor(cents % 25);
+let dimes = Math.floor( remainderDimes/10);
+console.log( 'dimes =' + dimes);
+
+let remainderNickle = Math.floor(cents % 10);
+let nickles = Math.floor (remainderNickle / 5);
+console.log('nickles =' + nickles);
+
+let remainderPennies = Math.floor(cents % 5);
+let pennies = Math.floor(remainderPennies / 1);
+console.log ('pennies =' + pennies);
